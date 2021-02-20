cc4262e9e842f9cc6cdfdb9ff8f4285042b05c13
Rosie-g <ruz0408@gmail.com>
2021-02-20 11:27:19 -0800

    finished my user story

--- src/test/java/com/libraryCT/pages/BasePage.java
+++ src/test/java/com/libraryCT/pages/BasePage.java
@@ -11,4 +11,14 @@ public abstract class BasePage {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
+
+    @FindBy(xpath = "//span[.='Dashboard']")
+    public WebElement dashboardLink;
+
+    @FindBy(xpath = "//span[.='Users']")
+    public WebElement usersLink;
+
+    @FindBy(xpath = "//span[.='Books']")
+    public WebElement booksLink;
+
 }
