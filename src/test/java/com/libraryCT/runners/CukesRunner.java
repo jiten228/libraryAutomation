0205e7e3c26fd753a4dc2566b878e0f33d542482
ChristinaDT <74195190+ChristinaDT@users.noreply.github.com>
2021-02-21 21:39:59 -0600

    New Commit @WIP

--- src/test/java/com/libraryCT/runners/CukesRunner.java
+++ src/test/java/com/libraryCT/runners/CukesRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "com/libraryCT/step_definitions",
-        tags = "@groupcategory",
+        tags = "@groupCategory",
         dryRun = false
 )
 
