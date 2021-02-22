9ab17df2305caadb84b8bfcfe17ec3f2aca14433
delaram1978 <delaram78a@gmail.com>
2021-02-21 20:55:59 -0600

    pages commit

--- src/test/java/com/libraryCT/pages/BorrowingBooksPage.java
+++ src/test/java/com/libraryCT/pages/BorrowingBooksPage.java
@@ -1,4 +1,27 @@
 package com.libraryCT.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class BorrowingBooksPage extends BasePage{
+
+
+    @FindBy(xpath ="//*[@id=\"borrowed_list\"]/thead/tr/th[1]")
+     public WebElement Action;
+
+    @FindBy(xpath =" //*[@id=\"borrowed_list\"]/thead/tr/th[2]")
+    public WebElement BookName;
+
+    @FindBy(xpath ="//*[@id=\"borrowed_list\"]/thead/tr/th[3]")
+    public WebElement BorrowedData;
+
+    @FindBy(xpath = "//*[@id=\"borrowed_list\"]/thead/tr/th[4]")
+    public WebElement PlannedReturnedData;
+
+   @FindBy(xpath = "//*[@id=\"borrowed_list\"]/thead/tr/th[5]")
+    public WebElement ReturnData;
+
+   @FindBy(xpath = "//*[@id=\"borrowed_list\"]/thead/tr/th[6]")
+    public WebElement IsReturned;
+
 }
--- src/test/java/com/libraryCT/runners/CukesRunner.java
+++ src/test/java/com/libraryCT/runners/CukesRunner.java
@@ -8,8 +8,8 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "com/libraryCT/step_definitions",
-        tags = "@addUser",
-        dryRun = false
+        tags = "@wip",
+        dryRun = true
 )
 
 public class CukesRunner {
