e1d638409505fadea2caedf08535886a82fcd4e2
jiten228 <jiten228@yahoo.com>
2021-02-22 22:49:36 -0600

    Test 3 story on Jenkins

--- src/test/java/com/libraryCT/runners/CukesRunner.java
+++ src/test/java/com/libraryCT/runners/CukesRunner.java
@@ -6,6 +6,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = {
+                "html:target/cucumber-report.html",
+                "json:target/cucumber.json",
+                "rerun:target/rerun.txt"
+        },
         features = "src/test/resources/features",
         glue = "com/libraryCT/step_definitions",
 
